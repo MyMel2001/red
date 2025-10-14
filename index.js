14f42a605134b92aee66edc1677db6d8554557bd
sparky <no@nope.nicetry>
2025-10-14 01:59:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -228,7 +228,7 @@ const IE5_STYLES = `
     }
     .main-col {
         float: left; 
-        width: 30%; /* Adjusted to leave room for the nav and side columns */
+        width: 44%; /* Adjusted to leave room for the nav and side columns */
         padding: 0 10px;
         padding-left: 20px;
         padding-right: 12px;
